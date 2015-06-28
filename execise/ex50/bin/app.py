d62ac7424ccf95e44dd7287f19b32efeb0c21ad8
xxulai <xulai_taobao@hotmail.com>
2015-06-28 16:49:57 +0800

    ex50

---
+++ execise/ex50/bin/app.py
@@ -0,0 +1,18 @@
+#-*-coding :utf-8-*-
+import web
+
+urls = (
+	'/', 'index'
+)
+
+app=web.application(urls, globals())
+
+render = web.template.render('templates/')
+
+class index:
+	def GET(self):
+		greeting = "Hello World"
+		return render.index(greeting)
+
+if __name__ == "__main__":
+	app.run()
