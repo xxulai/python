1a97834cf3e55cdcf8719ead797e92a6884d2983
xxulai <xulai_taobao@hotmail.com>
2015-07-11 15:08:13 +0800

    ex52 finish

--- execise/ex52/bin/app.py
+++ execise/ex52/bin/app.py
@@ -1,28 +1,54 @@
 #-*-coding :utf-8-*-
 import web
+from gothonweb import map
 
 urls = (
-	'/hello', 'index'
+	'/game', 'GameEngine',
+	'/', 'Index'
 )
+
+'''
+if in this way
+urls = (
+	'/game', 'GameEngine' !!!!!!!!!!!! no ',' here, there will be 1 object missing!!!!! then will keep reporting --> ValueError: need more than 1 value to unpack
+	'/', 'Index'
+)
+'''
 
 app=web.application(urls, globals())
 
-#render = web.template.render('templates/')
+#hack to make debug mode works with sessions
+if web.config.get('_session') is None:
+	store = web.session.DiskStore('sessions')
+	session = web.session.Session(app, store, initializer={'room':None})
+	web.config._session = session
+else:
+	session = web.config._session
+	
 render = web.template.render('templates/', base='layout') #use the template templates/layout.html
 
-class index:
-	def GET(self):
-		return render.hello_form()
-		'''form = web.input(name = "Nobody", greet = None)
-		if form.greet:
-			greeting = "%s, %s" % (form.greet, form.name)
-			return render.index(greeting = greeting)
-		else:
-			return "ERROR: greet is required"'''
-	def POST(self):
-		form = web.input(name="Nobody", greet="Hello")
-		greeting = "%s, %s" % (form.greet, form.name)
-		return render.index(greeting = greeting)
+class Index(object):
 
+	def GET(self):
+		#this is used to "setup" the session with starting values
+		session.room = map.START
+		web.seeother("/game")
+		
+class GameEngine(object):
+	
+		def GET(self):
+			if session.room:
+				return render.show_room(room=session.room)
+			else:
+				return render.you_died()
+		
+		def POST(self):
+			form = web.input(action=None)
+			
+			if session.room and form.action:
+				session.room = session.room.go(form.action)
+
+			web.seeother("/game")
+			
 if __name__ == "__main__":
 	app.run()
---
+++ execise/ex52/settings.cmd
@@ -0,0 +1,2 @@
+set PYTHONPATH=C:\Users\aaa\Documents\GitHub\python\execise\ex52
+echo Set current path to PYTHONPATH
